086e5e17daca27dd02fbbc5a911dc4bf375c1248
Alexi <alexi@contacto@alexidev.cl>
2019-10-01 14:21:08 -0300

    Added parameters to db connection

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 //middleware
 
 //db connection
-mongoose.connect(process.env.DB_URL,(err,res) => {
+mongoose.connect(process.env.DB_URL,{useNewUrlParser : true, useUnifiedTopology : true},(err,res) => {
     if (err) throw err;
 
     console.log('db connected');
